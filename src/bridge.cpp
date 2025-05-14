090080f5a7fe7afc769d3ec45f500bb34a5a4a7b
George Florea Bănuș <georgefb899@gmail.com>
2025-05-15 01:53:49 +0300

    bridge: use uint

--- src/bridge.cpp
+++ src/bridge.cpp
@@ -45,8 +45,8 @@ QString Bridge::parentPath(QString path)
 
 QString Bridge::thumbSaveLocation()
 {
-    auto picturesFolder{QStandardPaths::writableLocation(QStandardPaths::PicturesLocation)};
-    auto saveFolder{u"%1/rina"_s.arg(picturesFolder)};
+    auto picturesFolder {QStandardPaths::writableLocation(QStandardPaths::PicturesLocation)};
+    auto saveFolder {u"%1/rina"_s.arg(picturesFolder)};
     QDir dir;
     dir.mkpath(saveFolder);
     return saveFolder;
@@ -72,23 +72,23 @@ void ThumbnailerRunnable::run()
         return;
     }
 
-    auto rows {RinaSettings::self()->thumbnailsRows()};
-    auto columns {RinaSettings::self()->thumbnailsColumns()};
-    auto thumbWidth {RinaSettings::self()->thumbnailsWidth()};
-    auto spacing {RinaSettings::self()->thumbnailsSpacing()};
-    auto fileDuration {frameDecoder.getDuration()};
+    uint rows {static_cast<uint>(RinaSettings::self()->thumbnailsRows())};
+    uint columns {static_cast<uint>(RinaSettings::self()->thumbnailsColumns())};
+    uint thumbWidth {static_cast<uint>(RinaSettings::self()->thumbnailsWidth())};
+    uint spacing {static_cast<uint>(RinaSettings::self()->thumbnailsSpacing())};
+    uint fileDuration {static_cast<uint>(frameDecoder.getDuration())};
 
-    auto aspectRatio {static_cast<float>(frameDecoder.getWidth())/frameDecoder.getHeight()};
-    auto thumbHeight {thumbWidth/aspectRatio};
+    float aspectRatio {static_cast<float>(frameDecoder.getWidth())/frameDecoder.getHeight()};
+    uint thumbHeight {static_cast<uint>(thumbWidth/aspectRatio)};
 
-    auto w {(columns * thumbWidth) + (spacing * columns + spacing)};
-    auto h {(rows * thumbHeight) + (spacing * rows + spacing)};
-    auto totalThumbs {rows * columns};
-    auto startTime {static_cast<float>(fileDuration)/totalThumbs};
+    uint w {(columns * thumbWidth) + (spacing * columns + spacing)};
+    uint h {(rows * thumbHeight) + (spacing * rows + spacing)};
+    uint totalThumbs {rows * columns};
+    float startTime {static_cast<float>(fileDuration)/totalThumbs};
 
     QStringList files;
     QImage image;
-    uint seekPosition{0};
+    uint seekPosition {0};
 
     QTemporaryDir tmpDir;
     if (!tmpDir.isValid()) {
@@ -96,7 +96,7 @@ void ThumbnailerRunnable::run()
     }
 
     uint prevSeekPosition {0};
-    for (int i = 0; i < totalThumbs; ++i) {
+    for (uint i {0}; i < totalThumbs; ++i) {
         seekPosition = static_cast<uint>(startTime * i);
         if (i == 0) {
             seekPosition = static_cast<uint>(startTime / 2);
@@ -116,18 +116,19 @@ void ThumbnailerRunnable::run()
         prevSeekPosition = seekPosition;
     }
 
-    uint x{0};
-    QImage thumbsImage ({w, static_cast<int>(h)}, QImage::Format_RGB32);
+    uint x {0};
+    QImage thumbsImage ({static_cast<int>(w), static_cast<int>(h)}, QImage::Format_RGB32);
     QImage thumbImg;
     thumbsImage.fill(QColor::fromString(RinaSettings::self()->backgroundColor()));
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < columns; ++j) {
-            auto left {(j * thumbWidth) + ((j + 1) * spacing)};
-            auto top {(i * thumbHeight) + ((i + 1) * spacing)};
+    for (uint i {0}; i < rows; ++i) {
+        for (uint j {0}; j < columns; ++j) {
+            uint left {(j * thumbWidth) + ((j + 1) * spacing)};
+            uint top {(i * thumbHeight) + ((i + 1) * spacing)};
             thumbImg.load(files.at(x));
 
             QPainter p(&thumbsImage);
-            p.drawImage(QRect{left, static_cast<int>(top), thumbImg.width(), thumbImg.height()}, thumbImg);
+            p.drawImage(QRect{static_cast<int>(left), static_cast<int>(top), thumbImg.width(), thumbImg.height()},
+                        thumbImg);
 
             QFile::moveToTrash(files.at(x));
 
