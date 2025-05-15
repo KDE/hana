57c6c77e0d0617fdd510b53c8fdb3871f57193b7
George Florea Bănuș <georgefb899@gmail.com>
2025-05-15 23:08:57 +0300

    bridge: add file info to the final image

--- src/bridge.cpp
+++ src/bridge.cpp
@@ -7,6 +7,7 @@
 #include <QPainter>
 #include <QStandardPaths>
 #include <QTemporaryDir>
+#include <QTextDocument>
 
 #include "framedecoder.h"
 #include "settings.h"
@@ -127,6 +128,7 @@ void ThumbnailerRunnable::run()
 
     uint x {0};
     QImage thumbsImage ({static_cast<int>(w), static_cast<int>(h)}, QImage::Format_RGB32);
+    QPainter p(&thumbsImage);
     QImage thumbImg;
     thumbsImage.fill(QColor::fromString(RinaSettings::self()->backgroundColor()));
     for (uint i {0}; i < rows; ++i) {
@@ -135,7 +137,6 @@ void ThumbnailerRunnable::run()
             uint top {(i * thumbHeight) + ((i + 1) * spacing)};
             thumbImg.load(files.at(x));
 
-            QPainter p(&thumbsImage);
             p.drawImage(QRect{static_cast<int>(left), static_cast<int>(top), thumbImg.width(), thumbImg.height()},
                         thumbImg);
 
@@ -144,8 +145,38 @@ void ThumbnailerRunnable::run()
             x++;
         }
     }
+
+    QFileInfo fi(m_url.toLocalFile());
+    QFont font;
+    font.setPointSize(20);
+
+    QString html;
+    html.append(u"<div><strong>%1</strong></div>"_s.arg(m_url.fileName()));
+    html.append(u"<div><strong>Size</strong>  %1%1</div>"_s.arg(fi.size()));
+    html.append(u"<div><strong>Resolution</strong> %1x%2</div>"_s.arg(frameDecoder.getWidth()).arg(frameDecoder.getHeight()));
+    html.append(u"<div><strong>Codec</strong> %1</div>"_s.arg(frameDecoder.getCodec()));
+    html.append(u"<div><strong>Length</strong> %1</div>"_s.arg(frameDecoder.getDuration()));
+
+    uint docPadding{20};
+    QTextDocument td;
+    td.setDefaultFont(font);
+    td.setHtml(html);
+    td.setTextWidth(w - docPadding * 2);
+    td.setDocumentMargin(docPadding);
+
+    QImage textImage(QSize{static_cast<int>(thumbsImage.width()), static_cast<int>(td.size().height())}, QImage::Format_RGB32);
+    textImage.fill(QColor(Qt::gray));
+    QPainter textPainter(&textImage);
+    td.drawContents(&textPainter, QRectF{0, 0, td.textWidth(), td.size().height()});
+
+    QImage thumbsImageWithText(thumbsImage.width(), thumbsImage.height() + textImage.height(), QImage::Format_RGB32);
+    thumbsImageWithText.fill(QColor::fromString(RinaSettings::self()->backgroundColor()));
+    QPainter thumbsImageWithTextPainter(&thumbsImageWithText);
+    thumbsImageWithTextPainter.drawImage(QRect{0, 0, textImage.width(), textImage.height()}, textImage);
+    thumbsImageWithTextPainter.drawImage(QRect{0, textImage.height(), thumbsImage.width(), thumbsImage.height()}, thumbsImage);
+
     auto thumbsImagePath {u"%1/%2.thumbs.png"_s.arg(m_saveFolder).arg(m_url.fileName())};
-    thumbsImage.save(thumbsImagePath);
+    thumbsImageWithText.save(thumbsImagePath);
     Q_EMIT done(m_url.isLocalFile() ? m_url.toLocalFile() : QString{}, thumbsImagePath);
     Q_EMIT thumbnailProgress(m_url.isLocalFile() ? m_url.toLocalFile() : QString{}, 100);
     qDebug() << "Finished" << thumbsImagePath << "in" << timer.elapsed() << "miliseconds";
