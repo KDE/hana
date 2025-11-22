dc0da7d05c57dcdd3e5b811049fe499c042b3756
George Florea Bănuș <georgefb899@gmail.com>
2025-11-23 01:01:47 +0200

    main: setup KAboutData

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,11 @@
 #include <QQmlContext>
 #include <QQmlExtensionPlugin>
 
+#include <KAboutData>
 #include <KLocalizedQmlContext>
 #include <KLocalizedString>
+
+#include "hana-version.h"
 
 using namespace Qt::StringLiterals;
 
@@ -23,6 +26,25 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
     KLocalizedString::setApplicationDomain("hana");
     KLocalization::setupLocalizedContext(&engine);
+
+    KAboutData aboutData;
+    aboutData.setComponentName(u"hana"_s);
+    aboutData.setDisplayName(u"Hana"_s);
+    aboutData.setVersion(HANA_VERSION_STRING);
+    aboutData.setShortDescription(i18nc("@title", "Video thumbnails generator"));
+    aboutData.setLicense(KAboutLicense::GPL_V3);
+    aboutData.setCopyrightStatement(i18nc("copyright statement, use copyright symbol and en dash for the year range", "© 2025"));
+    aboutData.setHomepage(u"https://invent.kde.org/multimedia/hana"_s);
+    aboutData.setBugAddress("https://bugs.kde.org/enter_bug.cgi?product=Hana");
+    aboutData.setDesktopFileName(u"org.kde.hana"_s);
+
+    aboutData.addAuthor(i18nc("@info:credit", "George Florea Bănuș"),
+                          i18nc("@info:credit", "Developer"),
+                          u"georgefb899@gmail.com"_s,
+                          u"https://georgefb.com"_s);
+
+    KAboutData::setApplicationData(aboutData);
+
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
                      &app, []() { QCoreApplication::exit(-1); },
     Qt::QueuedConnection);
